d2077a703d6ed76acad7abe46cf5d6f053da75a8
izbid <bede.chk@gmail.com>
2024-01-15 23:04:26 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Get Weather
+
+This script opens makes an api call to a weather service and obtains the weather of particular location.
+
+It logs the the returned response into a weather_status log file
---
+++ weather.py
@@ -0,0 +1,33 @@
+import logging
+import logging.handlers
+import os
+import requests
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logger_file_handler = logging.handlers.RotatingFileHandler(
+    "weather_status.log",
+    maxBytes=1024 * 1024,
+    backupCount=1,
+    encoding="utf8",
+)
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+logger_file_handler.setFormatter(formatter)
+logger.addHandler(logger_file_handler)
+
+try:
+    api_TOKEN = os.environ["API_TOKEN"]
+except KeyError:
+    API_TOKEN = "Token not available!"
+ 
+
+if __name__ == "__main__":
+    logger.info(f"Token value: {API_TOKEN}")
+
+    r = requests.get('https://weather.talkpython.fm/api/weather/?city=Berlin&country=DE')
+    if r.status_code == 200:
+        data = r.json()
+        print(data)
+        temperature = data["forecast"]["temp"]
+        weather = data["weather"]["description"]
+        logger.info(f'Weather in Berlin: {weather},temp: {temperature}')
